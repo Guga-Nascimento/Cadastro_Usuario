26ac0acf8f5dcfb1b80a67ad249731069ec3e9a9
Guga-Nascimento <guganascimento001@gmail.com>
2023-07-21 16:02:01 -0300

    integração com banco de dados

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '198825';
+$dbName = 'formulariogn';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//if($conexao->connect_errno)
+//{
+//    echo"Erro";
+//}
+//else
+//{
+//    echo"conexão efetuada com sucesso";
+//}
+
+?>
--- formulario.php
+++ formulario.php
@@ -1,3 +1,22 @@
+<?php
+
+if(isset($_POST['submit']))
+{
+include_once('config.php');
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$sexo = $_POST['genero'];
+$data_nasc = $_POST['data_nascimento'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$endereco = $_POST['endereco'];
+
+$result = mysqli_query($conexao, "INSERT INTO cadastro(nome,email,telefone,genero,data_nasc,cidade,estado,endereco)
+                                  VALUES('$nome','$email','$telefone','$sexo','$data_nasc','$cidade','$estado','$endereco')");
+}
+?>    
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -84,7 +103,7 @@
 </head>
 <body>
     <div class="box">
-        <form action="">
+        <form action="formulario.php" method="POST">
             <fieldset>
                 <legend><strong>Formulário de Clientes</strong></legend>
                 <br>
